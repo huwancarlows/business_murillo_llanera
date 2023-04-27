1f41638ae81bb67ff54da40e847c4c2b005ebafc
huwancarlows <murillojuancarlos499@gmail..com>
2023-04-28 00:50:55 +0800

    8th Edit with login

---
+++ script.js
@@ -0,0 +1,8 @@
+let sgnn = document.getElementById("sgnn");
+let lgnn = document.getElementById("lgnn");
+let nmfld = document.getElementById("nmfld");
+let ttl = document.getElementById("ttl");
+
+lgnn.onclick = function(){
+    nmfld.style.maxHeight = "0";
+}
